a6c29153fc5ebdabde3b4598a8266291675d3161
vangona <rlarhksrud14@gmail.com>
2022-10-15 19:42:20 +0900

    Feature: 스타일 객체 만들어서 스타일 적용해보기

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Canvas 연습해보기
+# React & Canvas 연습해보기
 
 ## React에서 canvas 실습해보기
 
@@ -6,3 +6,8 @@
 - [x] React Canvas로 뭐라도 그려보기
 - [] React Canvas로 애니메이션 만들어보기
 - [] React Canvas에서 키보드 이벤트로 움직이는 사물 그리기
+
+## React Style 실습해보기
+
+- [] 기본 스타일 적용해보기
+- [x] 스타일 객체로 만들어서 적용해보기
--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -1,10 +1,20 @@
-import React, { Ref, RefObject, useEffect, useRef } from "react";
+import React, { RefObject, useEffect, useRef, useState } from "react";
+
+const buttonBoxStyle = {
+  background: "black",
+};
 
 const Map: React.FC = () => {
+  const [clickAnimation, setClickAnimation] = useState(1);
+  const buttonBox = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const onClickCanvas = () => {
     console.log("맵 클릭");
+  };
+
+  const onClickButton = (e: Event) => {
+    console.log(e.target);
   };
 
   const drawRecangle = (canvasRef: RefObject<HTMLCanvasElement>) => {
@@ -17,6 +27,9 @@ const Map: React.FC = () => {
   };
 
   useEffect(() => {
+    if (buttonBox.current) {
+      buttonBox.current.addEventListener("onclick", onClickButton);
+    }
     // ref가 설정되면 그 이후에 제어한다.
     if (canvasRef.current) {
       drawRecangle(canvasRef);
@@ -24,12 +37,19 @@ const Map: React.FC = () => {
   }, []);
 
   return (
-    <canvas
-      width={window.innerWidth}
-      height={window.innerHeight}
-      onClick={onClickCanvas}
-      ref={canvasRef}
-    ></canvas>
+    <>
+      <div style={buttonBoxStyle} className="buttonBox">
+        <button>1</button>
+        <button>2</button>
+        <button>3</button>
+      </div>
+      <canvas
+        width={window.innerWidth}
+        height={window.innerHeight}
+        onClick={onClickCanvas}
+        ref={canvasRef}
+      ></canvas>
+    </>
   );
 };
 
