2a78b351f45f1e73e36551eeceffa902f5ecb34b
vangona <rlarhksrud14@gmail.com>
2022-10-15 21:49:57 +0900

    Feature: React 애니메이션 선택기 만들기

--- src/Map/Map.tsx
+++ src/Map/Map.tsx
@@ -6,18 +6,18 @@ const buttonBoxStyle = {
 
 const Map: React.FC = () => {
   const [clickAnimation, setClickAnimation] = useState(1);
-  const buttonBox = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  const onClickCanvas = () => {
-    console.log("맵 클릭");
+  const onClickButtonBox: React.MouseEventHandler = (e) => {
+    const eventTarget = e.target as HTMLElement;
+    if (eventTarget.dataset.animation === "drawRedRect") {
+      setClickAnimation(1);
+    } else if (eventTarget.dataset.animation === "drawRedRectAnimation") {
+      setClickAnimation(2);
+    }
   };
 
-  const onClickButton = (e: Event) => {
-    console.log(e.target);
-  };
-
-  const drawRecangle = (canvasRef: RefObject<HTMLCanvasElement>) => {
+  const drawRectangle = (canvasRef: RefObject<HTMLCanvasElement>) => {
     const ctx = canvasRef.current!.getContext("2d")!;
     ctx.beginPath();
     ctx.rect(20, 40, 50, 50);
@@ -26,22 +26,31 @@ const Map: React.FC = () => {
     ctx.closePath();
   };
 
-  useEffect(() => {
-    if (buttonBox.current) {
-      buttonBox.current.addEventListener("onclick", onClickButton);
+  const onClickCanvas = () => {
+    console.log("맵 클릭! 애니메이션 : ", clickAnimation);
+    switch (clickAnimation) {
+      case 1:
+        drawRectangle(canvasRef);
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
     }
-    // ref가 설정되면 그 이후에 제어한다.
-    if (canvasRef.current) {
-      drawRecangle(canvasRef);
-    }
-  }, []);
+  };
 
   return (
     <>
-      <div style={buttonBoxStyle} className="buttonBox">
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
+      <div
+        onClick={onClickButtonBox}
+        style={buttonBoxStyle}
+        className="buttonBox"
+      >
+        <button data-animation="drawRedRect">직사각형 그리기</button>
+        <button data-animation="drawRedRectAnimation">
+          직사각형 그리기 애니메이션
+        </button>
+        <button>애니메이션</button>
       </div>
       <canvas
         width={window.innerWidth}
